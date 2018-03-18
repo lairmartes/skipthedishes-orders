fbc55117a4e218e69b8d355db30fa4fdbb1a7e56
lairmartes <lairmartes@gmail.com>
2018-03-18 14:26:28 -0300

    First commit in orders webservices

---
+++ src/main/java/com/martialdev/hackaton/skipthedishes/orders/SkipthedishesOrdersApplication.java
@@ -0,0 +1,12 @@
+package com.martialdev.hackaton.skipthedishes.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkipthedishesOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkipthedishesOrdersApplication.class, args);
+	}
+}
