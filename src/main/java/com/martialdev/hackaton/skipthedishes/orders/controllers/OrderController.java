b4ed28e2d384ac54345afe1a15c5af1df8a3be66
lairmartes <lairmartes@gmail.com>
2018-03-19 01:25:20 -0300

    All features working

--- src/main/java/com/martialdev/hackaton/skipthedishes/orders/controllers/OrderController.java
+++ src/main/java/com/martialdev/hackaton/skipthedishes/orders/controllers/OrderController.java
@@ -1,12 +1,15 @@
 package com.martialdev.hackaton.skipthedishes.orders.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.webmvc.PersistentEntityResource;
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,7 @@ import com.martialdev.hackaton.skipthedishes.orders.repos.OrderRepository;
 public class OrderController {
 	
 	@Autowired
-	OrderRepository mealOrderRepository;
+	OrderRepository orderRepository;
 	
 	@PostMapping("/")
 	public ResponseEntity<PersistentEntityResource> save(@RequestBody Order mealOrder, PersistentEntityResourceAssembler assembler) {
@@ -28,18 +31,34 @@ public class OrderController {
 		
 		System.out.println(mealOrder.getTotal());
 		
-		 mealOrderRepository.save(mealOrder);
+		orderRepository.save(mealOrder);
 
 		return ResponseEntity.ok(assembler.toResource(mealOrder));
 	}
 
-	@GetMapping("/{id}")
-	public ResponseEntity<PersistentEntityResource> get(@Param("id") String id, PersistentEntityResourceAssembler assembler) {
+	@GetMapping("/status/{id}")
+	public ResponseEntity<String> get(@PathVariable Long id) {
 		
-		Order order = new Order();
+		Optional<Order> askOrder  = orderRepository.findById(id);
+		Order order = askOrder.get();
+		if (!askOrder.isPresent()) {
+			throw new ResourceNotFoundException("Order not found");
+		}
 		
-		return ResponseEntity.ok(assembler.toResource(order));
+		return ResponseEntity.ok("order " +order.getId() + " " + order.getStatus());
 		
+	}
+	
+	@PostMapping("/cancel/{id}")
+	public ResponseEntity<String> save(@PathVariable Long id) {
+		
+		int qttyRowsAffected = orderRepository.cancelOrder(id);
+		
+		if (qttyRowsAffected < 1) {
+			throw new ResourceNotFoundException("Order not found");
+		}
+
+		return ResponseEntity.ok("order " + id + " has been cancelled");
 	}
 
 }
--- src/main/java/com/martialdev/hackaton/skipthedishes/orders/repos/OrderRepository.java
+++ src/main/java/com/martialdev/hackaton/skipthedishes/orders/repos/OrderRepository.java
@@ -1,9 +1,19 @@
 package com.martialdev.hackaton.skipthedishes.orders.repos;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.martialdev.hackaton.skipthedishes.orders.entities.Order;
 
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+	
+	@Modifying
+	@Transactional
+	@Query("update Order o set o.status = 'cancelled' where o.id = :id")
+	public int cancelOrder(@Param("id") Long id);
 
 }
